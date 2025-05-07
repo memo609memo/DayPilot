8f1d76c9e4516c7ca23d837b7de21786aa40f09d
Michael Dibella <memodibella@icloud.com>
2025-05-07 14:45:50 -0400

    Created basic layout for register fragment

--- app/src/main/java/com/example/daypilot/AuthActivity.kt
+++ app/src/main/java/com/example/daypilot/AuthActivity.kt
@@ -2,20 +2,11 @@ package com.example.daypilot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.daypilot.ui.login.LoginFragment
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-    }
-
-    fun switchToLoginFragment() {
-
-    }
-
-    fun switchToRegisterFragment() {
-
     }
 
 
