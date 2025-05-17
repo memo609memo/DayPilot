6d5c96044ef144e3a6c7bb4ebbff1c1ded734fb4
Michael Dibella <memodibella@icloud.com>
2025-05-17 17:52:35 -0400

    Recommit for splash screen logic

--- app/src/main/java/com/example/daypilot/SplashActivity.kt
+++ app/src/main/java/com/example/daypilot/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.example.daypilot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+
+    private val splashDelay: Long = 2500
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, AuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashDelay)
+
+
     }
 }
