96a976a2c6dd854a748dece657c98f949fb5085e
Luis Orellana Benitez <LEOrellanaBenitez@student.fullsail.edu>
2025-05-21 18:14:07 -0500

    first iteration of calendar UI done, the calendar can save dates in the viewModel, method to add icons to each day still in process, date formattin method added YYYY-Mmm-dd

--- app/src/main/java/com/example/daypilot/ui/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/daypilot/ui/notes/NotesViewModel.kt
@@ -3,9 +3,25 @@ package com.example.daypilot.ui.notes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.applandeo.materialcalendarview.EventDay
+import java.util.Collections.emptyList
 
 class NotesViewModel : ViewModel() {
-   private  val  _text = MutableLiveData<String>().apply {
-      value = "This is Notes Fragment " }
-   val  text : LiveData<String> = _text
+   private  val  _selectedDate = MutableLiveData<String>()
+   val selectedDate: LiveData<String> = _selectedDate
+
+   private val _eventList = MutableLiveData<List<EventDay>>(emptyList())
+   val eventList: LiveData<List<EventDay>> = _eventList
+   fun selectedDate(dateString: String)
+   {
+      _selectedDate.value = dateString
+   }
+
+   fun addEvent(event:EventDay){
+
+      val current = _eventList.value?.toMutableList() ?: mutableListOf()
+      current.add(event)
+      _eventList.value = current
+   }
+
 }
