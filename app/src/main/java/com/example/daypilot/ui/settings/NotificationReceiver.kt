610deae488f734e53c0328bbe349727d27cf7e99
Ryan Reese <RAReese1@student.fullsail.edu>
2025-07-06 15:33:33 -0500

    Modified notifications functionality to add a notification tap feature that launches the app. Added functionality to delete all scheduled notifications upon account logout and deletion. Also modified where other key notification/settings functions were called when app is ran. Lastly added functionality to make sure all notifications are re-scheduled when phone is rebooted

---
+++ app/src/main/java/com/example/daypilot/ui/settings/NotificationReceiver.kt
@@ -0,0 +1,52 @@
+package com.example.daypilot.ui.settings
+
+import android.Manifest
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.daypilot.MainActivity
+import com.example.daypilot.R
+
+const val notificationID = 121
+const val channelID = "channel1"
+const val titleExtra = "titleExtra"
+const val messageExtra = "messageExtra"
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        Log.d("Debugging", "NotificationReceiver Fired")
+
+        val launchIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            launchIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notification = NotificationCompat.Builder(context, channelID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(intent.getStringExtra(titleExtra))
+            .setContentText(intent.getStringExtra(messageExtra))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(System.currentTimeMillis().toInt(), notification.build())
+        }
+    }
+}
